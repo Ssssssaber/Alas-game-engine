86bb3f73feb479cf9b71320c9d283676cdec69a3
Ssssssaber <bladimir.lobanov@gmail.com>
2024-11-04 01:55:21 +0700

    (feature) base sdl2 integration

--- AlasCore/src/Core/Application.cpp
+++ AlasCore/src/Core/Application.cpp
@@ -1,6 +1,7 @@
 #include "Application.h"
 #include "Events/ApplicationEvent.h"
 #include "Core/Logger.h"
+#include "SDL.h"
 namespace AGS
 {
 
@@ -10,17 +11,28 @@ namespace AGS
     
     void Application::Run()
     {
-        WindowResizeEvent e(1024, 768);
-        if (e.isInCategory(EventCategoryApplication))
+        // while (1);
+        SDL_Window *window = nullptr;
+
+        if (SDL_Init(SDL_INIT_VIDEO) < 0)
         {
-            // AGS_CORE_INFO(e);
-            AGS_CORE_INFO("e");
+            AGS_CORE_ERROR(SDL_GetError());
         }
-        if (e.isInCategory(EventCategoryInput))
+        else
         {
-            AGS_CORE_INFO("e");
+            window = SDL_CreateWindow("Main Window", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 480, SDL_WINDOW_SHOWN);
+            if (window == NULL)
+            {
+                AGS_CORE_ERROR(SDL_GetError());
+            }
+            else
+            {
+                SDL_UpdateWindowSurface(window);
+                SDL_Delay(200);
+            }
         }
         
-        while(1); 
+        SDL_DestroyWindow(window);
+        SDL_Quit();
     }
 }
