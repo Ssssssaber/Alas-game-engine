7cffdea191193ab238b38ac9c256e48eae5c155e
Ssssssaber <bladimir.lobanov@gmail.com>
2024-11-03 15:02:32 +0700

    (fix) logging fix

--- AlasCore/src/Core/EntryPoint.h
+++ AlasCore/src/Core/EntryPoint.h
@@ -7,6 +7,9 @@ extern AGS::Application* AGS::CreateApplication();
 int main(int argv, char** argc)
 {
     auto app = AGS::CreateApplication();
+    AGS::Logger::Init();
+    AGS_CLIENT_INFO("KEKE IS YOU");
+    AGS_CORE_WARN("NOT BABA IS YOU");
     app->Run();
     delete app;
 }
--- AlasCore/src/Core/Logger.h
+++ AlasCore/src/Core/Logger.h
@@ -2,7 +2,7 @@
 
 #include <memory>
 
-#include "../../../vendor/spdlog/include/spdlog/spdlog.h"
+#include "spdlog/spdlog.h"
 #include "spdlog/sinks/stdout_color_sinks.h"
 
 
@@ -23,3 +23,16 @@ namespace AGS {
     };
 
 }
+
+#define AGS_CORE_FATAL(...) ::AGS::Logger::GetCoreLogger()->fatal(__VA_ARGS__)
+#define AGS_CORE_ERROR(...) ::AGS::Logger::GetCoreLogger()->error(__VA_ARGS__)
+#define AGS_CORE_WARN(...) ::AGS::Logger::GetCoreLogger()->warn(__VA_ARGS__)
+#define AGS_CORE_INFO(...) ::AGS::Logger::GetCoreLogger()->info(__VA_ARGS__)
+#define AGS_CORE_TRACE(...) ::AGS::Logger::GetCoreLogger()->error(__VA_ARGS__)
+
+
+#define AGS_CLIENT_FATAL(...) ::AGS::Logger::GetClientLogger()->fatal(__VA_ARGS__)
+#define AGS_CLIENT_ERROR(...) ::AGS::Logger::GetClientLogger()->error(__VA_ARGS__)
+#define AGS_CLIENT_WARN(...) ::AGS::Logger::GetClientLogger()->warn(__VA_ARGS__)
+#define AGS_CLIENT_INFO(...) ::AGS::Logger::GetClientLogger()->info(__VA_ARGS__)
+#define AGS_CLIENT_TRACE(...) ::AGS::Logger::GetClientLogger()->error(__VA_ARGS__)
