e33e41c2f119c09cb6e7c18faa1e3e34533acf9c
Ssssssaber <bladimir.lobanov@gmail.com>
2024-12-16 17:58:30 +0700

    (fix) assert opengl only in debug mode

--- AlasCore/src/Platform/OpenGL/OpenGLCore.h
+++ AlasCore/src/Platform/OpenGL/OpenGLCore.h
@@ -1,20 +1,25 @@
 #include <glad/glad.h>
 
-#define GlCall(x)  GLClearError();\
-    x;\
-    AGS_CORE_ASSERT(GLLogCall(#x, __FILE__, __LINE__), "OPEN GL ERROR"); 
+#ifdef AGS_ENABLE_ASSERTS
+    #define GlCall(x)  GLClearError();\
+        x;\
+        AGS_CORE_ASSERT(GLLogCall(#x, __FILE__, __LINE__), "OPEN GL ERROR"); 
 
-static void GLClearError()
-{
-    while(glGetError() != GL_NO_ERROR);
-}
-
-static bool GLLogCall(const char* function, const char* file, int line)
-{
-    while (GLenum error = glGetError())
+    static void GLClearError()
     {
-        AGS_CORE_ERROR("[OPENGL ERROR]: (code: {0}) {1}; {2}; {3}", error, function, file, line);
-        return false;
+        while(glGetError() != GL_NO_ERROR);
     }
-    return true;
-}
+
+    static bool GLLogCall(const char* function, const char* file, int line)
+    {
+        while (GLenum error = glGetError())
+        {
+            AGS_CORE_ERROR("[OPENGL ERROR]: (code: {0}) {1}; {2}; {3}", error, function, file, line);
+            return false;
+        }
+        return true;
+    }
+#else
+    #define GlCall(x) {}
+#endif
+
